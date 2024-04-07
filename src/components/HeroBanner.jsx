6b5954d6320b799b4353e3e5db108640ee9306be
KStar-coder <sharmakaustav04@gmail.com>
2024-04-07 20:44:53 +0530

    2nd hero image

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,23 +4,21 @@ import { Box, Stack, Typography } from '@mui/material';
 import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '1300px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+  <div className='py-4 px-2 md:py-2 md:px-4 text-center md:text-left'>
+    <h1 className='text-4xl font-bold text-red-500 font-sans'>Fitness Club</h1>
+    <p className='text-4xl font-bold text-black font-sans'>
       Sweat, Smile <br />
       And Repeat
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+    </p>
+    <p className='text-xl text-pretty mb-3'>
       Check out the most effective exercises personalized to you
-    </Typography>
-    <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
-    </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
-  </Box>
+    </p>
+    <p>
+      <a href="#exercises" className='m-4 mb-4 text-center text-2xl font-semibold bg-red-500 rounded-md p-2 text-white' >Explore Exercises</a>
+    </p>
+
+    <img src={HeroBannerImage} alt="hero-banner" className='mt-4 w-80 md:w-auto' />
+  </div>
 );
 
 export default HeroBanner;
